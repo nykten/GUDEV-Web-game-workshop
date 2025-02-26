5fc03ba2f23e04ae08da479ba0c3a41b90ea6338
Nik Harith <2673209b@student.gla.ac.uk>
2025-02-26 19:45:51 +0000

    Add Matter engine

--- game.js
+++ game.js
@@ -21,12 +21,21 @@ document.body.appendChild(app.canvas);
 
 app.stage.scale.set(60);
 
+
+const engine = Matter.Engine.create({
+  gravity: {
+    x: 0,
+    y: 0.1,
+
+  },
+});
+
 const world = new PIXI.Container();
 world.position.set(app.screen.width / 2 / app.stage.scale.x, app.screen.height / 2 / app.stage.scale.y);
 
 app.stage.addChild(world);
 
-function createSprite(x, y, width, height, color) {
+function createSprite(x, y, width, height, color, physicsOptions = { isStatic: false, frictionAir: 0.01, group: 0, isSensor: false}, angle = 0) {
   const sprite = new PIXI.Container();
   sprite.position.set(x, y);
   sprite.pivot.set(width / 2, height / 2);
@@ -38,14 +47,60 @@ function createSprite(x, y, width, height, color) {
   sprite.addChild(graphics);
   world.addChild(sprite);
 
-  return sprite;
+  let body = null;
+  if (physicsOptions) {
+    body= Matter.Bodies.rectangle(x, y, width, height, {
+      ...physicsOptions, // compression mehtod
+      collisionFilter: {
+        group: physicsOptions.group,
+      },
+    });
+
+    console.log(angle);
+    Matter.Body.setAngle(body, angle);
+    Matter.World.add(engine.world, body);
+
+    Matter.Events.on(engine, "beforeUpdate", () => {
+      sprite.position.set(body.position.x, body.position.y);
+      sprite.rotation = body.angle;
+    });
+  }
+
+  return [sprite, body];
 }
 
-const playerSprite = createSprite(0, 0, 1, 1, 0x2dc455)
+const [playerSprite, playerBody] = createSprite(0, 0, 1, 1, 0x1f781a);
+let canDoubleJump = false;
+
+createSprite(0, 3, 8, 1, 0x1a781b, {isStatic: true, group: 1});
+
+function isOnFloor() {
+  const collisions = Matter.Query.ray(
+    engine.world.bodies,
+    playerBody.position,
+    Matter.Vector.add(playerBody.position, { x: 0, y: 1 + 0.1}),
+  );
+
+  return collisions.some((collisions) => collisions.bodyA.collisionFilter.group === 1);
+}
+
+window.addEventListener("keydown", (e) => {
+  if (e.code !== "Space") {
+    return;
+  }
+
+  const isPlayerOnFloor = isOnFloor();
+  if (isPlayerOnFloor) {
+    canDoubleJump = true;
+    Matter.Body.setVelocity(playerBody, {x: playerBody.velocity.x, y: 0.5});
+  } else if (canDoubleJump) {
+    canDoubleJump = false;
+    Matter.Body.setVelocity(playerBody, {x: playerBody.velocity.x, y: 0.5});
+  }
+})
 
 function update({ deltaTime, deltaMS }) {
   // playerSprite.rotation += 0.05 * deltaTime;
-
   let playerDirection = new PIXI.Point(0, 0);
   
   if (key.get("KeyW")) {
@@ -63,8 +118,16 @@ function update({ deltaTime, deltaMS }) {
 
   if (playerDirection.magnitude()) {
     playerDirection = playerDirection.normalize();
-    playerSprite.position = playerSprite.position.add(playerDirection.multiplyScalar(0.2 * deltaTime));
+    // playerSprite.position = playerSprite.position.add(playerDirection.multiplyScalar(0.12 * deltaTime));
+    Matter.Body.setVelocity(playerBody, {
+      x: playerDirection.x * 0.2,
+      y: playerBody.velocity.y,
+    });
   }
+
+  Matter.Engine.update(engine);
+
+  // const cameraSpeed = ;
 }
 
 
